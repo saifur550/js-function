986ec2c060d8d904a849ecb4f4f272f9f9f59072
saifur550 <srstap550@gmail.com>
2021-08-03 09:34:07 +0800

    git init

---
+++ js/function.js
@@ -0,0 +1,61 @@
+// function btnClick(){
+//     var thanks = document.querySelector('h2');
+//     if(thanks.innerHTML == 'Thank You'){
+//         thanks.innerHTML =' "I truly appreciate your hard work"' 
+//     }else{
+//         thanks.innerHTML ='Thank You'
+//     }
+// }
+
+
+// function startFan(){
+//     console.log('here is first call');
+//     console.log('here is second call');
+// }
+
+
+// //call the function
+// startFan();
+// console.log('here is first call');
+// console.log('here is second call');
+// console.log('here is first call');
+// console.log('here is second call');
+// console.log('here is first call');
+// console.log('here is second call');
+// console.log('here is first call');
+// console.log('here is second call');
+// console.log('here is first call');
+// console.log('here is second call');
+// startFan();
+
+
+// function itemValue(item){
+//     console.log('display will show item number ',item);
+// }
+
+
+// var itemName = 80
+// itemValue(itemName)
+
+
+// function greetmessage (){
+//     console.log("hello there");
+// }
+
+// greetmessage();
+
+
+var  heading = document.querySelector('h2');
+ 
+// function greetmessage (name){
+//    heading.innerHTML= "Greeting Name :- " + name;
+// }
+// var name = prompt("Enter a Your ");
+// greetmessage(name);
+
+
+function add(a,b){
+    heading.innerHTML=(a+b);
+}
+add(3,4);
+add(5,4);
---
+++ js/main.js
@@ -0,0 +1,45 @@
+// array
+
+// var favoriteBook = '4 hour work week';
+// var bookList =['position ','hooked' , 'start with why ' , ' path of life ', ' in the last journay '];
+
+// var bookName= bookList.indexOf('path of life ');
+// bookList[2] = 'story brand';
+// bookList.push('small giants');
+// bookList.pop();
+
+
+
+// if conditionals
+
+// if(bookList[1]=='hooked'){
+//     console.log(bookList[1]);
+//     // document.write(bookList[1]);
+//     // document.getElementById('demo').innerHTML=bookList[1];
+// }else{
+//     console.log('is not listed book');
+// }
+
+
+
+//while loop
+
+// var itemCount = 0;
+
+// while(itemCount <= 20){
+//    document.getElementById('demo').innerHTML = itemCount ;
+//    itemCount++;
+// }
+
+
+
+//for loop 
+
+
+// for (var i = 0; i < 15; i++){
+//     // document.getElementById('demo1').innerHTML = i ;
+//     console.log(i);
+// }
+
+
+
